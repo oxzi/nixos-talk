a1c5a4867ab41d0250b18858b542f8cf8a0c967a
geistesk <github.jyf300hj@0x21.biz>
2017-11-03 14:03:19 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Nix and NixOS-Talk
+
+## Build
+```bash
+git clone THIS_REPO_URL --recursive
+nix-build
+# result/ will contain the complete presentation
+```
---
+++ default.nix
@@ -0,0 +1,32 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+let
+  revealjs-version = "3.5.0";
+in pkgs.stdenv.mkDerivation {
+  name = "nixos-talk";
+  src = ./.;
+
+  nativeBuildInputs = with pkgs; [ git pandoc ];
+
+  patchPhase = ''
+    cd reveal.js/
+    git checkout ${revealjs-version}
+    cd ..
+  '';
+
+  buildPhase = ''
+    pandoc \
+      --standalone \
+      --incremental \
+      --to revealjs \
+      --output index.html \
+      talk.md
+  '';
+
+  installPhase = ''
+    mkdir $out
+    cp index.html $out/
+    cp -r img $out/
+    cp -r reveal.js $out/
+  '';
+}
